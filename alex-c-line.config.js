3d1c2d35aa2721bfb872d9f195d7f461c5c48e5e
AlexMan123456 <alex.man03888@gmail.com>
2026-03-01 14:49:29 +0000

    Remove artwork from this repository
    It is now contained in the media repository.

--- alex-c-line.config.js
+++ alex-c-line.config.js
@@ -1,15 +1,3 @@
 import { packageConfig } from "alex-c-line/configs/internal";
-import path from "node:path";
 
-const artworkPath = path.join(process.cwd(), "artwork");
-
-export default packageConfig([
-  "format",
-  async (stepRunner) => {
-    await stepRunner({ cwd: artworkPath })`pdm run format`;
-  },
-  "lint",
-  async (stepRunner) => {
-    await stepRunner({ cwd: artworkPath })`pdm run lint`;
-  },
-]);
+export default packageConfig(["format", "lint"]);
